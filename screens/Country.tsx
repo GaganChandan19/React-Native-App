a170b3d0668c7179a6a42620ec0514c12dbb137c
GaganChandan19 <gagan.chandan@cognitiveclouds.com>
2023-03-04 14:38:56 +0530

    Updated with Country page and Weather Page

--- screens/Country.tsx
+++ screens/Country.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Text, View, Image, Button } from 'react-native';
+import {  DataTable } from 'react-native-paper';
 
-const Country = () => {
-  return (
-    <View>
-        <Text>Welcome</Text>
-    </View>
-  )
+
+const Country = ({navigation, route }:any) => {
+    let country = route.params.country;
+    country=country.trim();
+
+    interface IData {
+      capital: string;
+      latlng: any;
+      population: number;
+      flags:any
+  }
+
+  const [{ capital, latlng, population, flags }, setdata] = useState<IData>({ capital: '', latlng: [], population: 0, flags: {} });
+    useEffect(() => {
+        fetch(`https://restcountries.com/v2/name/${country}?fullText=true`)
+            .then((res) => res.json())
+            .then((res) => setdata(res[0]))
+            .catch((err) => console.log(err))
+    }, [country]);
+
+
+    return (
+        <View style={{flex:1,justifyContent:'center',alignItems:'center', backgroundColor:'black'}}>
+            <Text style={{ fontSize: 25, marginBottom: 30, color:'white' }}>Country Information</Text>
+                     <View  style={{ borderWidth: 1, height: 250,backgroundColor:'white', alignItems: 'center', justifyContent: 'center', padding:35 , borderRadius: 10 }}>
+                     <Image
+                            style={{width:150,height:80}}
+                            source={{uri: `${flags.png}`}}
+                        />
+                        <Text>Capital: {capital}</Text>
+                        <Text>Population: {population}</Text>
+                        <Text>Latitude: {latlng[0]}</Text>
+                        <Text>Longitude: {latlng[1]}</Text>
+                        <Button title="Weather" onPress={()=>navigation.navigate('Weather',{"capital":capital})}></Button>
+                    </View>  
+        </View>
+    )
 }
 
 export default Country
--- screens/Weather.tsx
+++ screens/Weather.tsx
@@ -1,12 +1,65 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useState } from "react";
+import { Image, Text, View } from "react-native";
 
-const Weather = () => {
+const Weather = ({ navigation, route }: any) => {
+  const { capital } = route.params;
+  interface IData {
+    temperature: number;
+    weather_icons: any;
+    wind_speed: number;
+    precip: number;
+  }
+
+  const [{ temperature, weather_icons, wind_speed, precip }, setdata] =
+    useState<IData>({
+      temperature: 0,
+      weather_icons: [],
+      wind_speed: 0,
+      precip: 0,
+    });
+
+  useEffect(() => {
+    fetch(
+      `http://api.weatherstack.com/current?access_key=68f83f417142a947f0a6b9f6f1f427d8&query=${capital}`
+    )
+      .then((res) => res.json())
+      .then((res) => setdata(res.current))
+      .catch((err) => console.log(err));
+  }, [capital]);
+
   return (
-    <View>
-        <Text>Welcome</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+      }}
+    >
+      <Text style={{ fontSize: 25, marginBottom: 30, color: "white" }}>
+        Weather
+      </Text>
+      <View
+        style={{
+          borderWidth: 1,
+          height: 250,
+          backgroundColor: "white",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 35,
+          borderRadius: 10,
+        }}
+      >
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{ uri: `${weather_icons}` }}
+        />
+        <Text>Temperature: {temperature}</Text>
+        <Text>Wind Speed: {wind_speed}</Text>
+        <Text>Precip: {precip}</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
